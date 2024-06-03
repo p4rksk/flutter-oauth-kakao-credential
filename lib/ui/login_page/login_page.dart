4f81c03a1084f6aea9d46ff4280de2aee67422df
yeeun0118s <yeeun0118s@naver.com>
2024-06-03 15:31:53 +0900

    카카오 로그인 적용 완료

--- lib/ui/login_page/login_page.dart
+++ lib/ui/login_page/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
+import '../../_core/http.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
@@ -12,18 +14,35 @@ class LoginPage extends StatelessWidget {
         children: [
           ElevatedButton(
             onPressed: () async {
-              try {
-                OAuthToken token = await UserApi.instance
-                    .loginWithKakaoAccount();
-                print('카카오톡으로 로그인 성공 ${token.accessToken}');
-              } catch (error) {
-                print('카카오톡으로 로그인 실패 $error');
-              }
+              kakaoLogin();
             },
             child: Text("카카오 로그인"),
           )
         ],
       ),
     );
+  }
+}
+
+void kakaoLogin() async{
+  try {
+    // 1. 크리덴셜 로그인 - 토근 받기
+    OAuthToken token = await UserApi.instance
+        .loginWithKakaoAccount();
+    // qRR-UeowO-LEp0_KXAGH41vyG64QiCkJAAAAAQopyV8AAAGP3Jb1gxamEcnPBcmr
+    print('카카오톡으로 로그인 성공 ${token.accessToken}');
+
+    // 2. 토큰(카카오)을 스프링 서버에 전달하기 (스프링 서버에게 나 인증했어! 라고 알려주는 과졍)
+    final response = await dio.get("/oauth/callback", queryParameters: {"accessToken":token.accessToken});
+
+    // 3. 토큰(스프링 서버) 응답 받기
+    final blogAccessToken = response.headers["Authorization"]!.first;
+    print("blogAccessToken : ${blogAccessToken}");
+
+
+    // 4. 시큐어 스토리지에 저장
+    secureStorage.write(key: blogAccessToken, value: blogAccessToken);
+  } catch (error) {
+    print('카카오톡으로 로그인 실패 $error');
   }
 }
